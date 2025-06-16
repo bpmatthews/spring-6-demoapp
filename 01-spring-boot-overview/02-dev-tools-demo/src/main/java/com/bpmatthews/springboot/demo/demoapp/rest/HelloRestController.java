e4e6705b4d9d6b4904580f2cbf8369b8b335e93b
bpmatthews <bpmatthews@users.noreply.github.com>
2025-06-15 19:29:41 -0500

    Add Spring 6 demo project files

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/bpmatthews/springboot/demo/demoapp/rest/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.bpmatthews.springboot.demo.demoapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    // expose "/" that returns "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
