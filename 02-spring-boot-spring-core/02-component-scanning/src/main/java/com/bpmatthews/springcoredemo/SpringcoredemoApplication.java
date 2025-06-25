1770da057643dafc47793b268427de4f54fc26b8
bpmatthews <bpmatthews@users.noreply.github.com>
2025-06-24 23:58:15 -0500

    Practice default and explicit component scanning with custom packages

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/bpmatthews/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,19 @@
+package com.bpmatthews.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication(
+		scanBasePackages = {"com.bpmatthews.springcoredemo",
+					        "com.bpmatthews.util"})
+*/
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
