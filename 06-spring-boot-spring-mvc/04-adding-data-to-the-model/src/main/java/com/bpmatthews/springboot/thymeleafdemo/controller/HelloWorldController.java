52860e01323891490c0838e525113ce01a3ec1a9
bpmatthews <bpmatthews@users.noreply.github.com>
2025-08-04 21:16:21 -0500

    Edited controller to add data to model

---
+++ 06-spring-boot-spring-mvc/04-adding-data-to-the-model/src/main/java/com/bpmatthews/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,45 @@
+package com.bpmatthews.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    // need a controller method to show initial HTML form
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // need a controller method to process the HTML form
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and
+    // add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read request parameter from HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result)
+
+        return "helloworld";
+    }
+}
